87e8adbc3fe7c8ec85a5f5db6cd3f3e35c2d1629
Guest User <student@epicodus.com>
2015-08-03 15:48:52 -0700

    Update specs

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -1,23 +1,23 @@
 require('rspec')
 require('ping_pong')
 
-describe('Fixnum#ping_pong?') do
-  it("displays numbers 1 to user input") do
-    expect((9).ping_pong()).to(eq([1, 2, 3, 4, 5, 6, 7, 8, 9]))
-  end
-end
-
-describe('Fixnum#ping_pong?') do
-  it("any number divisible by 3 is replaced with ping") do
-    expect((5).ping_pong()).to(eq([1, 2, "ping", 4, 5]))
-  end
-end
-
-describe('Fixnum#ping_pong?') do
-  it("any number divisible by 5 is replaced with pong") do
-    expect((7).ping_pong()).to(eq([1, 2, "ping", 4, "pong", "ping", 7]))
-  end
-end
+# describe('Fixnum#ping_pong?') do
+#   it("displays numbers 1 to user input") do
+#     expect((9).ping_pong()).to(eq([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+#   end
+# end
+#
+# describe('Fixnum#ping_pong?') do
+#   it("any number divisible by 3 is replaced with ping") do
+#     expect((5).ping_pong()).to(eq([1, 2, "ping", 4, 5]))
+#   end
+# end
+#
+# describe('Fixnum#ping_pong?') do
+#   it("any number divisible by 5 is replaced with pong") do
+#     expect((7).ping_pong()).to(eq([1, 2, "ping", 4, "pong", "ping", 7]))
+#   end
+# end
 
 describe('Fixnum#ping_pong?') do
   it("any number divisible by 3 and 5 is replaced with ping-pong") do
